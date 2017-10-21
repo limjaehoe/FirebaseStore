d22739a227ed22b78faaf7d866641d5c09cbcfb4
limjaehoe <woghl1129@naver.com>
2017-10-22 01:15:52 +0900

    2017_10_22 1:15

--- app/src/main/java/lim/jhsoft/com/firebasestore/MainActivity.java
+++ app/src/main/java/lim/jhsoft/com/firebasestore/MainActivity.java
@@ -14,7 +14,10 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.Transaction;
+import com.google.firebase.firestore.WriteBatch;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,8 +29,11 @@ public class MainActivity extends AppCompatActivity {
     private Button read_button;
     private Button data_model;
     private Button add_data;
-    private Button a;
-
+    private Button update_data;
+    private Button transcation1;
+    private Button transcation2;
+    private Button transcation3;
+    private Button delete1;
 
 
     @Override
@@ -40,6 +46,14 @@ public class MainActivity extends AppCompatActivity {
         read_button = findViewById(R.id.read_button);
         data_model = findViewById(R.id.data_model);
         add_data = findViewById(R.id.add_data);
+        update_data = findViewById(R.id.update_data);
+        transcation1 = findViewById(R.id.transactions1);
+        transcation2 = findViewById(R.id.transactions2);
+        transcation3 = findViewById(R.id.transactions3);
+        delete1 = findViewById(R.id.delete1);
+
+
+
 
 
         write_button.setOnClickListener(new View.OnClickListener() {
@@ -139,11 +153,129 @@ public class MainActivity extends AppCompatActivity {
                                 Log.w(TAG, "Error writing document", e);
                             }
                         });
+            }
+        });
+
+        //업데이트..
+        update_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DocumentReference washingtonRef = db.collection("cities").document("LA");
+
+                // Set the "isCapital" field of the city 'DC'
+                washingtonRef
+                        .update("capital", false)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "DocumentSnapshot successfully updated!");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error updating document", e);
+                            }
+                        });
+            }
+        });
+
+        transcation1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final DocumentReference sfDocRef = db.collection("cities").document("SF");
+
+                db.runTransaction(new Transaction.Function<Void>() {
+                    @Override
+                    public Void apply(Transaction transaction) throws FirebaseFirestoreException {
+                        DocumentSnapshot snapshot = transaction.get(sfDocRef);
+                        double newPopulation = snapshot.getDouble("population") + 1;
+                        transaction.update(sfDocRef, "population", newPopulation);
+
+                        // Success
+                        return null;
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "Transaction success!");
+                    }
+                })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Transaction failure.", e);
+                            }
+                        });
 
             }
         });
 
+        transcation2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final DocumentReference sfDocRef = db.collection("cities").document("SF");
 
+                db.runTransaction(new Transaction.Function<Double>() {
+                    @Override
+                    public Double apply(Transaction transaction) throws FirebaseFirestoreException {
+                        DocumentSnapshot snapshot = transaction.get(sfDocRef);
+                        double newPopulation = snapshot.getDouble("population") + 1;
+                        if (newPopulation <= 1000000) {
+                            transaction.update(sfDocRef, "population", newPopulation);
+                            return newPopulation;
+                        } else {
+                            throw new FirebaseFirestoreException("Population too high",
+                                    FirebaseFirestoreException.Code.ABORTED);
+                        }
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<Double>() {
+                    @Override
+                    public void onSuccess(Double result) {
+                        Log.d(TAG, "Transaction success: " + result);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Transaction failure.", e);
+                    }
+                });
+            }
+        });
+
+        transcation3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WriteBatch batch = db.batch();
+                // Set the value of 'NYC'
+                // Update the population of 'SF'
+                DocumentReference laRef = db.collection("cities").document("LA");
+                batch.delete(laRef);
+
+
+            }
+        });
+
+        delete1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db.collection("cities").document("DC")
+                        .delete()
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "DocumentSnapshot successfully deleted!");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error deleting document", e);
+                            }
+                        });
+            }
+        });
 
 
 
